c8f32dda4c023541235f8c9d9e183238705da635
Muhammadamin <webhacker0100@gmail.com>
2022-08-30 00:13:14 +0500

    Update version and compile to nuxt js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,15 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: 'class',
-  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  content: [
+    "./components/**/*.{js,vue,ts}",
+    "./layouts/**/*.vue",
+    "./pages/**/*.vue",
+    "./plugins/**/*.{js,ts}",
+    "./nuxt.config.{js,ts}",
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-};
+}
